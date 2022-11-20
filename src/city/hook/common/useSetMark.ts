b56bbb7ef9722d1f31b965eb1fc8198baeeada0e
ipiyou <tao710803@gmail.com>
2022-11-20 23:11:54 +0900

    MarkDown 변환 설정

---
+++ src/city/hook/common/useSetMark.ts
@@ -0,0 +1,57 @@
+import { marked } from "marked";
+
+const renderer: { [n: string]: Function } = {
+  heading(text: string, level: 1 | 2 | 3 | 4 | 5 | 6) {
+    const a = "#";
+    return `<h${level}>${a.repeat(level)} ${text}</h${level}>`;
+  },
+  em(text: string) {
+    return `*<em>${text}</em>*`;
+  },
+  strong(text: string) {
+    return `**<strong>${text}</strong>**`;
+  },
+  br() {
+    return "<br />";
+  },
+};
+
+const tokenizer: any = {
+  blockquote() {},
+  hr() {},
+  list() {},
+  link() {},
+  image() {},
+  codespan() {},
+  del() {},
+};
+
+const useSetMark = () => {
+  const ApplyList = ["blockquote", "list", "hr", "link", "image"];
+
+  const DisableFeature = (Feature: string[]) => {
+    marked.use({
+      tokenizer,
+      renderer,
+    });
+    const F = { exec: function () {} };
+    var lexer = new marked.Lexer({}) as any;
+    Feature.map((e) => {
+      if (lexer) lexer.tokenizer.rules.block[e] = F;
+    });
+  };
+
+  const Mark = (str: string) => {
+    marked.setOptions(marked.getDefaults());
+    return marked.parse(str);
+  };
+
+  const DisableMark = (str: string) => {
+    DisableFeature(ApplyList);
+    return marked.parse(str);
+  };
+  
+  return [Mark, DisableMark];
+};
+
+export default useSetMark;
