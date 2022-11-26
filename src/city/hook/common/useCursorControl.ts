a8c03a56673a619db40a7f8c4758f609a5464b4f
ipiyou <tao710803@gmail.com>
2022-11-27 00:24:28 +0900

    커서 셋(아무것도 없거나 첫 번째로 위치를 못 움직이는  버그 수정)

--- src/city/hook/common/useCursorControl.ts
+++ src/city/hook/common/useCursorControl.ts
@@ -4,32 +4,35 @@ const useCursorControl = () => {
   const setCursorRange = (
     RefNode: HTMLElement | null,
     range: Range,
+    offZero: boolean,
     stat: { pos: number; done: boolean }
   ) => {
     if (!RefNode) return;
 
-    const currentText = RefNode.textContent && RefNode.innerText;
     if (stat.done) return range;
-    if (RefNode.innerHTML == "<br>") {
-      if (!--stat.pos) {
-        range.setStart(RefNode, 0);
-        stat.done = true;
-      }
-    }
-    if (!currentText) return range;
+    if (!RefNode.textContent) return range;
 
     let currentNode = null;
     if (RefNode.childNodes.length == 0) {
-      if (currentText.length >= stat.pos) {
+      if (RefNode.textContent.length >= stat.pos) {
         range.setStart(RefNode, stat.pos);
         stat.done = true;
       } else {
-        stat.pos = stat.pos - currentText.length;
+        stat.pos = stat.pos - RefNode.textContent.length;
       }
     } else {
       for (var i = 0; i < RefNode.childNodes.length && !stat.done; i++) {
         currentNode = RefNode.childNodes[i] as HTMLElement;
-        setCursorRange(currentNode, range, stat);
+        if (currentNode.innerHTML == "<br>") {
+          if (!--stat.pos) {
+            range.setStart(currentNode, 0);
+            stat.done = true;
+          }
+          continue;
+        }
+        setCursorRange(currentNode, range, offZero, stat);
+
+        if (!offZero && stat.done) range.setStart(RefNode.childNodes[i + 1], 0);
       }
     }
 
@@ -41,8 +44,14 @@ const useCursorControl = () => {
     stat: { pos: number; done: boolean }
   ) => {
     const sel = window.getSelection();
-    console.log(stat);
-    const Range = setCursorRange(RefNode, document.createRange(), stat);
+    const off = sel?.focusOffset;
+
+    const Range = setCursorRange(
+      RefNode,
+      document.createRange(),
+      off ? true : false,
+      stat
+    );
     if (!sel || !Range) return;
 
     console.log(Range);
