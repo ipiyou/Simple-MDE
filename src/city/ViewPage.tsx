bdcc11f6313321b9d292e2482484520c969e102c
ipiyou <tao710803@gmail.com>
2022-11-27 23:52:20 +0900

    remove past component

--- src/city/ViewPage.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-function ViewPage() {
-  return <_LiveUpdate />;
-}
-
-const _LiveUpdate = styled.div``;
-
-export default ViewPage;
--- src/city/components/Edit/EditBar.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-function EditBar() {
-  return <_Wrapper></_Wrapper>;
-}
-
-const _Wrapper = styled.div``;
-
-export default EditBar;
